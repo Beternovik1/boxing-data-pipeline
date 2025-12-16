30f6bfa126833c6d210b362d31d667a05328839a
Edgar <edgaralfarohernandez15@gmail.com>
2025-12-16 01:50:17 -0800

    ETL Fase 1: Extraccion y transformacion de las tablas HTML de Wikipedia

---
+++ main.py
@@ -0,0 +1,94 @@
+# --------------------------------------- ETL --------------------------------------------------
+# 1. LIBRERIAS
+import pandas as pd
+# Libreria para acceder al codigo HTTML de una pagina WEB, lo devuelve como un String gigante
+import requests 
+# Toma el texto gigante y lo envuelve para que parezca un archivo en memoria y pandas lo pueda leer
+from io import StringIO
+# Es el cable que conecta mongodb con python
+from pymongo import MongoClient
+
+# 2. CONSTANTES
+URL = "https://en.wikipedia.org/wiki/List_of_current_world_boxing_champions"
+
+# -------------------------------------- EXTRACTING ---------------------------------------------
+# 3. FUNCION DE EXTRACCION DE LA INFORMACION
+def extract_champions():
+    print(f"Conectando a {URL}")
+
+    # Web scrapping (disfraz) fingimos ser una persona normal usando Google Chrome en Windows 10 para 
+    # acceder a wikipedia y no nos nieguen el acceso
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"        
+    }
+
+    try:
+        # A. Haciendo la peticion GET a wikipedia
+        response = requests.get(URL, headers = headers)
+
+        # Verificando que tengamos acceso a la informacion con el status_code 200
+        # Si fuera 404: "No existe esa página".
+        # Si fuera 403: "Prohibido el paso (te caché que eres un robot)".
+        # Si fuera 500: "El servidor de Wikipedia se incendió".
+        if response.status_code == 200:
+            print("Descargando tablas...")
+
+            # B. Leeyendo el HTML
+            # Filtrando las tablas de boxeo donde aparezca la palabra clave "WBA"
+            # pd.read_html busca especificamente tablas (<table></table)
+            todas_las_tablas = pd.read_html(StringIO(response.text), match="WBA")
+
+            print(f"Hay {len(todas_las_tablas)} tablas.")
+
+            # Regresando la lista completa con todas las tablas buenas
+            return todas_las_tablas
+        else:
+            print(f'Error de conexion {response.status_code}')
+            return []
+    except Exception as e:
+        print(f"Error: {e}")
+        return []
+    
+# -------------------------------------- TRANSFORMING ---------------------------------------------
+def transform_data(lista_de_tablas):
+    # A. Slicing nuestra lista de tablas para descartar las ultimas 2 tablas que no nos sirven
+    lista_limpia = lista_de_tablas[:18]
+
+    # B. Uniendo todas las tablas en un solo DF
+    df_final = pd.concat(lista_limpia, ignore_index=True)
+    
+    # C. Limpiando el texto usando Regex (Expresiones Regulares)
+    # Explicación del regex '\[.*\]|\(.*\)':
+    # \[.*\]  -> Busca corchetes y todo lo que tengan dentro (ej: [15])
+    # |       -> O (OR)
+    # \(.*\)  -> Busca paréntesis y todo lo que tengan dentro (ej: (Super champion))
+    # Remplazamos todo eso por "" en el DF
+    df_final = df_final.replace(to_replace=r'\[.*\]|\(.*\)', value='', regex=True)
+    return df_final 
+
+if __name__ == "__main__":
+    mis_tablas = extract_champions()
+   #print(f"Prueba: Tengo una lista con {len(mis_tablas)} tablas dentro.")
+
+    if len(mis_tablas) > 0:
+        df_final = transform_data(mis_tablas)
+
+        print("Vista previa jeje")
+        print(df_final.head(16))
+
+        print ("Info df")
+        print(df_final.info())
+    else:
+        print("No se encontraron tablas jeje")
+
+
+
+
+# Creando un entorno virtual
+# python -m venv venv
+
+# Activar el entorno virtual
+# source venv/Scripts/activate
+
+# Comando para despertar docker
+# docker start mi-mongo
